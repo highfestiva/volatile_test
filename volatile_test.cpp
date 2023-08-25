0ed818ab645d6958012f8b6c152c5e346eb82abe
Jonte <highfestiva@gmail.com>
2023-08-25 14:49:56 +0200

    Move volatile counter into test struct
    Did not affect distribution on either platform in the slightest.

--- volatile_test.cpp
+++ volatile_test.cpp
@@ -3,8 +3,6 @@
 // Restarting the worker threads and signalling that they're done uses an atomic_int
 // which they each use an hardcoded prime (29 for one thread, 31 for the other). When the
 // sum is 60, we know that both threads are done.
-//
-// I noticed that contention gets way worse for global variables than members on x64.
 
 
 #include <atomic>
@@ -12,47 +10,51 @@
 #include <thread>
 
 
-volatile int x = 0;
 std::atomic_int prime_counter(0);
 
 
-void thread1() {
-    prime_counter += 29;
-    while (prime_counter >= 0) {
-        while (prime_counter == 29); // wait until other thread catches up
-        while (prime_counter == 60); // wait until released
-        for (int i = 0; i < 10; i++) {
-            x++;
-        }
-        prime_counter += 29;
-    }
-}
+struct ConcurrentTest {
+    volatile int x = 0;
 
-void thread2() {
-    prime_counter += 31;
-    while (prime_counter >= 0) {
-        while (prime_counter == 31); // wait until other thread catches up
-        while (prime_counter == 60); // wait until released
-        for (int i = 0; i < 10; i++) {
-            x++;
+    void thread1() {
+        prime_counter += 29;
+        while (prime_counter >= 0) {
+            while (prime_counter == 29); // wait until other thread catches up
+            while (prime_counter == 60); // wait until released
+            for (int i = 0; i < 10; i++) {
+                x++;
+            }
+            prime_counter += 29;
         }
-        prime_counter += 31;
     }
-}
+
+    void thread2() {
+        prime_counter += 31;
+        while (prime_counter >= 0) {
+            while (prime_counter == 31); // wait until other thread catches up
+            while (prime_counter == 60); // wait until released
+            for (int i = 0; i < 10; i++) {
+                x++;
+            }
+            prime_counter += 31;
+        }
+    }
+};
 
 
 int main() {
     int results[30] = {};
-    std::thread t1(&thread1);
-    std::thread t2(&thread2);
+    ConcurrentTest t;
+    std::thread t1(&ConcurrentTest::thread1, &t);
+    std::thread t2(&ConcurrentTest::thread2, &t);
 
     while (prime_counter != 60); // wait until both threads are ready
 
     for (int i = 0; i < 10000000; ++i) {
-        x = 0; // reset volatile counter
+        t.x = 0; // reset volatile counter
         prime_counter = 0; // release both threads
         while (prime_counter != 60); // wait until both ready
-        int y = x;
+        int y = t.x;
         if (y >= 0 && y < 30) {
             results[y]++; // register volatile counter
         }
